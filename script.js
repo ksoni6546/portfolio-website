b29bb17797c641862c1b51226197d32e70285a15
ksoni6546 <ksoni654600@gmail.com>
2026-05-19 13:43:03 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,85 @@
+function goToProjects(){
+
+    document
+    .getElementById("projects")
+    .scrollIntoView({
+
+        behavior: "smooth"
+    });
+}
+
+
+
+function goToContact(){
+
+    document
+    .getElementById("contact")
+    .scrollIntoView({
+
+        behavior: "smooth"
+    });
+}
+
+
+
+function submitForm(){
+
+    let name = document.getElementById("name").value;
+
+    let email = document.getElementById("email").value;
+
+    let message = document.getElementById("message").value;
+
+
+
+    if(name === "" || email === "" || message === ""){
+
+        alert("Please fill all fields");
+    }
+
+    else{
+
+        alert("Message Sent Successfully");
+
+        document.getElementById("name").value = "";
+
+        document.getElementById("email").value = "";
+
+        document.getElementById("message").value = "";
+    }
+}
+
+
+
+function showProject(type){
+
+    let title = "";
+
+    let desc = "";
+
+
+
+    if(type === "erp"){
+
+        title = "College ERP System";
+
+        desc =
+        "Contributed to the development of a Java-based College ERP System as part of a team project. Worked on the Student Dashboard module, frontend UI improvements, student record management and database connectivity using DBMS concepts.";
+    }
+
+
+
+    else if(type === "game"){
+
+        title = "Unity Endless Runner Game";
+
+        desc =
+        "Developed a 2D endless runner game using Unity Engine and C#. Implemented player movement, obstacle mechanics, collision detection, scoring system and UI features while improving gameplay functionality through debugging and testing.";
+    }
+
+
+
+    document.getElementById("projectTitle").innerText = title;
+
+    document.getElementById("projectDesc").innerText = desc;
+}
