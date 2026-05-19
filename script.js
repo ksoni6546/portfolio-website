13c3ab3017babc4f386b01b9710995348d933c38
ksoni6546 <ksoni654600@gmail.com>
2026-05-19 14:11:32 +0530

    Updated portfolio website design

--- script.js
+++ script.js
@@ -83,3 +83,25 @@ function showProject(type){
 
     document.getElementById("projectDesc").innerText = desc;
 }
+
+
+
+/* Typing Effect */
+
+let text = "Frontend Developer";
+
+let index = 0;
+
+function typeEffect(){
+
+    if(index < text.length){
+
+        document.getElementById("typing-text").innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeEffect,100);
+    }
+}
+
+typeEffect();
